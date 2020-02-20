c4fd4ce9b1d9f15e2042525ce0f1fcb9ab67f1d4
KamranAlipour <kalipour@eng.ucsd.edu>
2020-02-20 17:50:45 +0000

    add multiple result generator script to run in parallel

---
+++ MWCNN_code/run_generate-final-results-invivo_2_0.05.sh
@@ -0,0 +1,1 @@
+python main.py --model MWCNN --save MWCNN_DeNoising_norm --scale 15 --n_feats 64 --save_results --print_model --n_colors 1 --test_only --self_ensemble --resume -1 --pre_train experiment/MWCNN_DeNoising_backup/model/ --task_type denoising --generate True --gen_set /home/data500/invivo/invivo_2_0.05 --ext jpg --slide 10
---
+++ MWCNN_code/run_generate-final-results-invivo_2_0.10.sh
@@ -0,0 +1,1 @@
+python main.py --model MWCNN --save MWCNN_DeNoising_norm --scale 15 --n_feats 64 --save_results --print_model --n_colors 1 --test_only --self_ensemble --resume -1 --pre_train experiment/MWCNN_DeNoising_backup/model/ --task_type denoising --generate True --gen_set /home/phacou/MWCNNv2/data/invivo/invivo_2_0.10 --ext jpg --slide 10
