36e052919e2a1e6134e8cb54f68af58c797c9865
KamranAlipour <kalipour@eng.ucsd.edu>
2020-04-23 08:04:46 +0000

    cleanup after initiation

--- MWCNN_code/run_generate-final-results-invivo_1_1.00.sh
+++
@@ -1,1 +0,0 @@
-python main.py --model MWCNN --save MWCNN_DeNoising_norm --scale 15 --n_feats 64 --save_results --print_model --n_colors 1 --test_only --self_ensemble --resume -1 --pre_train experiment/MWCNN_DeNoising_backup/model/ --task_type denoising --generate True --gen_set /home/data500/invivo/invivo_1_1.00 --ext jpg --slide 10
